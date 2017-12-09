62379de20258a01870f06a23f02ae873fe4d9f2b
John LaRosa <john.larosa@apexlearning.com>
2017-12-09 01:01:27 -0800

    completed last tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -37,8 +37,6 @@ $(function() {
         });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -71,7 +69,6 @@ $(function() {
         })
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -79,6 +76,19 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('should contain at least one entry after loadFeed() is invoked', function(done) {
+
+            var feedsCount = $('.feed .entry').length;
+
+            expect(feedsCount).toBeGreaterThan(0);
+            done();
+        })
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
@@ -87,6 +97,27 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+        var firstFeedContent;
+        var secondFeedContent;
+
+        // only call done after loading a new index
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                firstFeedContent = $('.feed').has('h2')[0].innerText;
+            });
+
+            loadFeed(1, function() {
+                secondFeedContent = $('.feed').has('h2')[0].innerText;
+                done();
+            });
+        });
+
+        it('should change content when new feed is loaded', function(done) {
+
+            expect(firstFeedContent).not.toEqual(secondFeedContent);
+            done();
+        })
 
     });
 }());
